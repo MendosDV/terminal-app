73ce5bb1172900c45082ec38b2bc431d558e13d4
Mickael Riss <mendos.prod@gmail.com>
2024-02-08 00:15:10 -0500

    add buttons

--- src/components/Terminal/TerminalView.vue
+++ src/components/Terminal/TerminalView.vue
@@ -1,14 +1,11 @@
 <template>
-  <div class="terminal text-white rounded-2">
+  <div v-show="showTerminal" class="terminal text-white rounded-2">
     <div class="terminal-header rounded-top">
-      <div class="terminal-buttons d-flex align-items-center h-100">
-        <span class="rounded-circle"></span>
-        <span class="rounded-circle"></span>
-        <span class="rounded-circle"></span>
+      <div class="terminal-buttons d-flex align-items-center h-100 px-2 gap-2">
+        <span v-for="button in buttons" :key="button" :id="button" role="button" class="rounded-circle pointer" @click="button === 'red' ? toggleTerminal() : ''"></span>
       </div>
     </div>
     <div class="terminal-window">
-
     </div>
   </div>
 </template>
@@ -16,6 +13,17 @@
 <script>
 export default {
   name: 'TerminalApp',
+  data () {
+    return {
+      showTerminal: true,
+      buttons: [ 'red', 'yellow', 'green'],
+    }
+  },
+  methods: {
+    toggleTerminal () {
+      this.showTerminal = !this.showTerminal
+    }
+  },
 }
 </script>
 
@@ -31,20 +39,36 @@ export default {
       height: 8%;
 
       .terminal-buttons {
-        span {
-          width: 14px;
-          background-color: white;
-          height: 14px;
+        #red {
+          background-color: #FF3B48;
+        }
+
+        #yellow {
+          background-color: #FFC100;
+        }
+
+        #green {
+          background-color: #00D742;
         }
       }
     }
 
     @media screen and (max-width: 768px) {
       width: 100%;
+
+      span {
+        width: 10px;
+        height: 10px;
+      }
     }
 
     @media screen and (min-width: 1024px) {
       width: 80%;
+
+      span {
+        width: 14px;
+        height: 14px;
+      }
     }
   }
 </style>
