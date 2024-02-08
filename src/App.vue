d32182d1708355ff3e92b761e2caf2bf4a3c9665
Mickael Riss <mendos.prod@gmail.com>
2024-02-07 23:55:38 -0500

    start design terminal

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class="text-white">
-    Hello
+  <div class="container-md d-flex justify-content-center align-items-center h-100">
+    <TerminalApp />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  height: 100vh;
-  font-family: Ubuntu Mono,monospace;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  background: rgb(2,0,36);
-  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(26,26,105,1) 35%, rgba(0,212,255,1) 100%);
+<script>
+import TerminalApp from './components/Terminal/TerminalView.vue'
+
+export default {
+  name: 'App',
+  components: { TerminalApp }
 }
+</script>
+
+<style lang="scss">
+  #app {
+    height: 100vh;
+    width: 100vw;
+    font-family: Ubuntu Mono,monospace;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(26,26,105,1) 35%, rgba(0,212,255,1) 100%);
+  }
 </style>
---
+++ src/components/Terminal/TerminalView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="terminal text-white rounded-2">
+    <div class="terminal-header rounded-top">
+      <div class="terminal-buttons d-flex align-items-center h-100">
+        <span class="rounded-circle"></span>
+        <span class="rounded-circle"></span>
+        <span class="rounded-circle"></span>
+      </div>
+    </div>
+    <div class="terminal-window">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TerminalApp',
+}
+</script>
+
+<style lang="scss" scoped>
+  .terminal {
+    height: 55%;
+    background-color: #040405;
+    opacity: 0.9;;
+
+    .terminal-header {
+      background-color: #575151;
+      width: 100%;
+      height: 8%;
+
+      .terminal-buttons {
+        span {
+          width: 14px;
+          background-color: white;
+          height: 14px;
+        }
+      }
+    }
+
+    @media screen and (max-width: 768px) {
+      width: 100%;
+    }
+
+    @media screen and (min-width: 1024px) {
+      width: 80%;
+    }
+  }
+</style>
